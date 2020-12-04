2f18f3bd33bffc6d53c01e755dbe063945916e14
Herbert Tung <herberttung@hotmail.com>
2020-12-04 02:09:41 +0000

    PC req

--- src/routes/functions/recheckJobs.js
+++ src/routes/functions/recheckJobs.js
@@ -31,5 +31,28 @@ router.get('/', async (req, res) => {
             spread++
         })
     })
+    options = {
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/get/jira/issue/search?jql=project%20%3D%20PCR%20AND%20"1st%20level%20Approval"%20is%20EMPTY%20&maxResults=9999',
+        json: true
+    }
+    rp(options).then(($) => {
+        let spread = 0
+        $.issues.forEach((issue) => {
+            setTimeout(function(){
+                console.log('Rechecking issue: ' + issue.key)
+                let options2 = {
+                    method: 'POST',
+                    uri: 'http://' + process.env.LOCALHOST + ':' + process.env.PORT + '/emailapi/routes',
+                    body: {
+                        issue: issue,
+                        webhookEvent: 'jira:issue_created'
+                    },
+                    json: true
+                }
+                rp(options2)
+            }, 5000 * spread)
+            spread++
+        })
+    })
 })
 module.exports = router;
