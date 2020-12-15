02078595a322410706a404bf5632ba2dd837fb1d
Herbert Tung <herberttung@hotmail.com>
2020-12-15 09:37:12 +0000

    issueFields

---
+++ src/routes/ICW/AlertB4SiteVisit.js
@@ -0,0 +1,135 @@
+import express from 'express'
+import rp from 'request-promise'
+import queryString from 'query-string'
+import { getEmails, getFieldMapping, issueNames } from '../../functions/jiraAPI'
+
+require('dotenv').config()
+
+var router = express.Router();
+
+router.post('/', async (req, res) => {
+    res.send('done')
+
+    let mappedFields = await issueNames(req.body.issue.key)
+    mappedFields = mappedFields.fields
+
+    const caseNumber = req.body.issue.key
+    const serviceName = req.body.issue.fields.issuetype.name
+    const caseSubject = req.body.issue.fields.summary
+
+    const SurveyorInsightId = mappedFields['Surveyor'][0].match(/\(([-A-Z0-9]*)\)$/)[1]
+    console.log(SurveyorInsightId)
+
+    let options2 = {
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/get/jira/object/keyAttributeValue?Key=' + SurveyorInsightId + '&returnAttribute=mail',
+        json: true
+    }
+
+    let to = await rp(options2)
+
+    let cc = []
+
+    let bcc = []
+
+    let options = {
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/get/jira/issue/issueScreenFields?screenId=' + '11547',
+        json: true
+    }
+
+    let fields = await rp(options).then(($) => {
+        return $.map((item) => { return item.name })
+    })
+
+    console.log(fields)
+
+    let displayFields = {}
+
+    fields.forEach((name) => {
+        displayFields[name] = null
+
+        try {
+            displayFields[name] = mappedFields[name].value
+        } catch { }
+        if (!(displayFields[name] && displayFields[name].length > 0)) {
+            try {
+                displayFields[name] = mappedFields[name].name
+            } catch { }
+        }
+        if (!(displayFields[name] && displayFields[name].length > 0)) {
+            try {
+                displayFields[name] = mappedFields[name][0].match(/(.*) \(([-A-Z0-9]*)\)$/)[1]
+            } catch { }
+        }
+        if (!(displayFields[name] && displayFields[name].length > 0)) {
+            try {
+                displayFields[name] = mappedFields[name].map((item) => { return item.value }).join(',')
+            } catch { }
+        }
+        if (!(displayFields[name] && displayFields[name].length > 0)) {
+            try {
+                displayFields[name] = mappedFields[name]
+            } catch { }
+        }
+    })
+
+    let body = '<table border=1><tr><td>Fields Name</td><td>Value</td></tr>'
+    Object.keys(displayFields).forEach((fieldName) => {
+        body += '<tr><td>' + fieldName + '</td><td>' + (displayFields[fieldName] || '') + '</td></tr>'
+    })
+    body += '</table>'
+
+    const emailOptions = {
+        method: 'POST',
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.PORT + '/emailapi/email',
+        json: true,
+        body: {
+            from: process.env.DEFUALTSENDER,
+            to: to,
+            cc: cc,
+            bcc,
+            subject: "Alert for 1 day befor site visit - " + caseNumber + " - " + caseSubject + "",
+            html: `Dear All</br></br>
+
+            Alert for 1 day befor site visit.</br></br>
+
+            Reference Number : `+ caseNumber + `</br>
+            Summary : ` + caseSubject + `</br>
+            Service : `+ serviceName + `</br></br>` + body + `</br></br>
+
+            <a href="https://hgcitd.atlassian.net/browse/` + caseNumber + `">View request</a></br></br>
+
+`
+        }
+    }
+    rp(emailOptions)
+
+    options = {
+        method: 'POST',
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/get/jira/issue/CustomFieldID',
+        body: { name: 'AlertMsgB4SiteVisit' },//'Creator User Info' },
+        json: true
+    }
+
+    let customFieldID = await rp(options)
+        .then(($) => {
+            return $
+        })
+
+    options = {
+        method: 'POST',
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/set/jira/issue/updateIssue',
+        body: {
+            "updateIssue": {
+                "issueId": req.body.issue.key,
+                "fields": {
+                    [customFieldID]: 'Y'
+                }
+            }
+        },
+        json: true
+    }
+
+    rp(options)
+})
+
+module.exports = router;
--- src/routes/functions/recheckJobs.js
+++ src/routes/functions/recheckJobs.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import rp from 'request-promise'
-import queryString from 'query-string'
+import moment from 'moment-timezone'
 
 require('dotenv').config()
 
@@ -8,6 +8,7 @@ var router = express.Router();
 
 router.get('/', async (req, res) => {
     console.log('Rechecking Jobs')
+    res.send('Rechecking Jobs')
     let options = {
         uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/get/jira/issue/search?jql=project%20%3D%20"Self%20Service"%20AND%20((type%20%3D%20"Service%20Request"%20AND%20"1st%20level%20Approval"%20%20is%20EMPTY%20AND%20status%20%3D%20"Pending%20for%201st%20Approval")%20OR%20(type%20%3D%20Incident%20AND%20"Assigned%20Group"%20is%20EMPTY%20AND%20status%20%3D%20Open))&maxResults=9999',
         json: true
@@ -15,7 +16,7 @@ router.get('/', async (req, res) => {
     rp(options).then(($) => {
         let spread = 0
         $.issues.forEach((issue) => {
-            setTimeout(function(){
+            setTimeout(function () {
                 console.log('Rechecking issue: ' + issue.key)
                 let options2 = {
                     method: 'POST',
@@ -38,7 +39,7 @@ router.get('/', async (req, res) => {
     rp(options).then(($) => {
         let spread = 0
         $.issues.forEach((issue) => {
-            setTimeout(function(){
+            setTimeout(function () {
                 console.log('Rechecking issue: ' + issue.key)
                 let options2 = {
                     method: 'POST',
@@ -46,6 +47,54 @@ router.get('/', async (req, res) => {
                     body: {
                         issue: issue,
                         webhookEvent: 'jira:issue_created'
+                    },
+                    json: true
+                }
+                rp(options2)
+            }, 5000 * spread)
+            spread++
+        })
+    })
+
+    options = {
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/get/jira/issue/search?jql=project%20%3D%20ICW%20AND%20"Site%20Visit%20Date"%20%3D%20"' + moment.tz("Asia/Hong_Kong").add(1, 'days').format('YYYY-MM-DD') + '"%20AND%20(AlertMsgB4SiteVisit%20is%20EMPTY%20OR%20AlertMsgB4SiteVisit%20!~%20"Y")',
+        json: true
+    }
+    //console.log(options)
+    rp(options).then(($) => {
+        let spread = 0
+        $.issues.forEach((issue) => {
+            setTimeout(function () {
+                console.log('Sending 1 day before site visit alert to issue: ' + issue.key)
+                let options2 = {
+                    method: 'POST',
+                    uri: 'http://' + process.env.LOCALHOST + ':' + process.env.PORT + '/emailapi/ICW/AlertB4SiteVisit',
+                    body: {
+                        issue: issue,
+                    },
+                    json: true
+                }
+                rp(options2)
+            }, 5000 * spread)
+            spread++
+        })
+    })
+
+    options = {
+        uri: 'http://' + process.env.LOCALHOST + ':' + process.env.JIRAAPIPORT + '/get/jira/issue/search?jql=project%20%3D%20ICW%20AND%20"Site%20Visit%20Date"%20%3D%20"' + moment.tz("Asia/Hong_Kong").add(-1, 'days').format('YYYY-MM-DD') + '"%20AND%20(AlertMsgAfterSiteVisit%20is%20EMPTY%20OR%20AlertMsgAfterSiteVisit%20!~%20"Y")',
+        json: true
+    }
+    //console.log(options)
+    rp(options).then(($) => {
+        let spread = 0
+        $.issues.forEach((issue) => {
+            setTimeout(function () {
+                console.log('Sending 1 after day site visit alert to issue: ' + issue.key)
+                let options2 = {
+                    method: 'POST',
+                    uri: 'http://' + process.env.LOCALHOST + ':' + process.env.PORT + '/emailapi/ICW/AlertAfterSiteVisit',
+                    body: {
+                        issue: issue,
                     },
                     json: true
                 }
